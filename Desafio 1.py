1bdc8a589d94d407b3e583db2d5ed435caad733c
Eduarda Dantas <161851900+DantinhasMD@users.noreply.github.com>
2025-03-21 19:12:00 -0300

    Update Desafio 1.py

--- Desafio 1.py
+++ Desafio 1.py
@@ -1,69 +1,65 @@
 # Criar um Sistema bancário - deposito, saque e extrato
-print('Seja Bem Vindo ao Banco X!') # Abrir sistema, dar boas vindas e apresentar as opções 
+print('Seja bem-vindo ao Banco X!') 
 
-print(''' Opções:
-[S] Sacar
-[C] Consultar extrato
-[D] Depositar
-[X] Encerrar
+print('''Opções Disponíveis:
+[S] - Sacar
+[C] - Consultar extrato
+[D] - Depositar
+[X] - Encerrar''')
 
-''')
-saldo = 2755
-numero_de_saques = 0
+saldo = 0
 LIMITE_DE_SAQUES = 3
+numero_de_saques = 0
+limite_saque = 500
 
-while True: 
+while True:
+  opcao = input("Qual operação deseja realizar: ")
+  if opcao not in ['s','c','d','x']:
+    print('Digite uma opção válida')
+    continue
+  else: 
+    if opcao == 's':
+      saque = float(input("Informe quanto deseja sacar: "))
+      
+      excedeu_saque = numero_de_saques >= LIMITE_DE_SAQUES
 
-    opcao = input("Qual operação deseja realizar: ")
-    if opcao not in ['s', 'c', 'd' , 'x']:
-        print('Esta não é uma opção válida, tente novamente!')
+      excedeu_saldo = saldo < saque
+
+      if saque > limite_saque: 
+         print('O valor excede o limite da conta. Tente Novamente!')
+         continue 
+
+      elif excedeu_saldo:
+          print('Saldo insuficiente para concluir a operação')
+          continue
+
+      elif excedeu_saque:
+          print('O número de saques diários disponíveis já foi ultrapassado. Tente novamente amanhã!')
+          continue
+      
+      else: 
+        saldo -= saque
+        print(f'O valor de R$ {saque:.2f} foi debitado de sua conta. Extrato final de R$ {saldo:.2f}')
+        numero_de_saques += 1
         continue
 
-    else: 
-        if opcao == 's':
-            saque = float(input("Informe quanto deseja sacar: "))
+    elif opcao == 'c':
+      print(f'Seu extrato é de R$ {saldo:.2f}')
+      continue
 
-            excedeu_saque = numero_de_saques >= LIMITE_DE_SAQUES
+    elif opcao == 'd':
+      deposito = float(input('Informe quanto deseja depositar: '))
+      if deposito < 0:
+        print("O valor não foi identificado. Por favor, tente novamente!")
+        continue
+      
+      else:
+        saldo += deposito
+        print(f'Seu extrato atual é de R$ {saldo:.2f}')
+        continue
 
-            excedeu_saldo = saldo < saque
-
-            if saque > 500: # Permitir saques de até R$500
-                print('O valor excede o limite da conta. Tente Novamente!') 
-                continue # Informar que o limite para saque foi ultrapassado
-
-            elif excedeu_saldo: 
-                print('Saldo insuficiente para concluir a operação')
-                continue
-
-            elif excedeu_saque: # Permitir apenas 3 saques
-                print('O número de saques diários disponíveis já foi ultrapassado. Tente novamente amanhã!')
-                continue
-
-            else:
-                saldo -= saque
-                print(f'O valor de R$ {saque:.2f} foi debitado de sua conta. Extrato final de R$ {saldo:.2f}')
-                numero_de_saques += 1
-                continue
-
-        elif opcao == 'c':
-            print(f"Seu extrato é de R$ {saldo:.2f}")
-            continue
-
-        elif opcao == 'd':
-            deposito = float(input('Informe quanto deseja depositar: '))
-            if deposito > 0:
-                saldo += deposito
-                print(f'Seu extrato atual é de R$ {saldo:.2f}')
-                continue
-
-            else: 
-                print("O valor não foi identificado. Por favor, tente novamente!")
-                continue
-
-        else: 
-            print(f'Você optou por encerrar a operação! Seu extrato final é de R$ {saldo:.2f}')
-
-    print("Obrigado por utilizar nossos serviços!")
-    break
+    else:
+      print("Obrigado por utilizar nossos serviços!")
+      break
 
      
