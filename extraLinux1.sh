45105be5e707faaa6af9f263549a9c9dcf1e1286
Eduarda Dantas <161851900+DantinhasMD@users.noreply.github.com>
2025-04-11 17:31:06 -0300

    Update and rename extraLinux1 to extraLinux1.sh
    Mudanças

--- extraLinux1.sh
+++ extraLinux1.sh
@@ -40,3 +40,19 @@ chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
+
+------------------------------------------------------------------------------
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
